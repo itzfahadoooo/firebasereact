1a2a7417e4400a7026d60ffca8ee8625dbad0145
itzfahadoooo <foyindolapo@gmail.com>
2025-04-14 21:40:20 +0100

    Initial Commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1-yyNM80f7Kk6nOZjn9syMcT4XJsjoYw",
+  authDomain: "fir-react-8d8d8.firebaseapp.com",
+  projectId: "fir-react-8d8d8",
+  storageBucket: "fir-react-8d8d8.firebasestorage.app",
+  messagingSenderId: "825504683872",
+  appId: "1:825504683872:web:0aa76adf83c722d2ccb005",
+  measurementId: "G-HKFBQL6C0Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth=getAuth(app);
+
+export {app,analytics,auth};
+
